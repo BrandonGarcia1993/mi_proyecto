e90ea2e540c016d6986903d9249d477401e9ce14
BrandonGarcia <kinodertoten1993@yahoo.com>
2025-06-28 01:04:25 -0600

    dependencia uno

--- src/main/java/com/pl/App.java
+++ src/main/java/com/pl/App.java
@@ -1,13 +1,22 @@
 package com.pl;
 
-/**
- * Hello world!
- *
- */
+//librerias importantes para usar FLATLAF
+import com.formdev.flatlaf.*;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 public class App 
 {
     public static void main( String[] args ){
 
+        //codigo para que usemos el tema FLATLAF dentro de un try_catch
+        try {
+            UIManager.setLookAndFeel(new FlatDarculaLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+
+        //instancia para usar ventana principal
         new VentanaPrincipal();
 
     }

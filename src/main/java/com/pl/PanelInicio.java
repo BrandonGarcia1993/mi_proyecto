da2686368a55c4eb41ae2e0654196ec3338458ee
BrandonGarcia <kinodertoten1993@yahoo.com>
2025-06-28 01:34:18 -0600

    commit tres

--- src/main/java/com/pl/PanelInicio.java
+++ src/main/java/com/pl/PanelInicio.java
@@ -1,17 +1,21 @@
 package com.pl;
 
 import javax.swing.*;//clase swing para componentes graficos
+import java.awt.*;//clase para usar ediciones como font en tipo y tamano de letras
 
 public class PanelInicio extends JPanel{//clase PanelInicio con extension a JPanel para crear componentes del panel
 
     public PanelInicio(VentanaPrincipal ventana){//el panel inicio tendra como asociasion ventana principal
         setLayout(null);//la edicion de componentes es personalizable
 
-        //BOTONES PRINCIPALES
+        //COMPONENTES
+        JLabel lblLogo = new JLabel("PLAY LOG!");//label que contiene el nombre de la aplicacion
         JButton btnIngresar = new JButton("INGRESAR");//boton ingresar
         JButton btnRegistrar = new JButton("REGISTRAR");//boton registrar
 
-        //EDICION DE BOTONES
+        //EDICION DE COMPONENTES
+        lblLogo.setFont(new Font("Arial", Font.BOLD,50));//edicion de las letras del nombre del logo
+        lblLogo.setBounds(270, 150, 320, 50);//dimensiones del label que contiene el nombre del logo
         btnIngresar.setBounds(340, 260, 120, 30);//dimensiones de boton ingresar
         btnRegistrar.setBounds(340, 220, 120, 30);//dimensiones de boton registrar
 
@@ -20,6 +24,7 @@ public class PanelInicio extends JPanel{//clase PanelInicio con extension a JPan
         btnRegistrar.addActionListener(e ->JOptionPane.showMessageDialog(null, "Registro exitoso!"));//funsiones de boton registrar
 
         //AGREGAR COMPONENTES
+        add(lblLogo);//agregamos el label logo
         add(btnIngresar);//agregamos boton ingresar
         add(btnRegistrar);//agregamos boton registrar
     }
